5a7db321c7d41252982c8d747d73c856154369d9
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-25 22:52:57 +0600

    Delete and Update Method Done and Functionality Completed

--- index.js
+++ index.js
@@ -35,10 +35,35 @@ async function run() {
         res.send(result);
     })
 
+    app.get("/users/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const user = await userCollection.findOne(query);
+        res.send(user);
+    })
+
     app.post("/users", async(req, res) => {
         const user = req.body;
         console.log("new user", user);
         const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+    app.put("/users/:id", async(req, res) => {
+        const id = req.params.id;
+        const user = req.body;
+        console.log(id, user);
+
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert: true};
+        const updatedUser = {
+            $set: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        const result = await userCollection.updateOne(filter, updatedUser, options);
         res.send(result);
     })
 
@@ -49,6 +74,7 @@ async function run() {
         const result = await userCollection.deleteOne(query);
         res.send(result);
     })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
