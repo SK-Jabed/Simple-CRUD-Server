0903e9ad273c21d6d8d6658e0bade0995ed51bfc
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-30 23:11:27 +0600

    Simple CRUD Server Done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
